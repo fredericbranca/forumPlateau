1732a50dd5d394cb6ca5b679bab377e4b7152138
Frederic Branca <frederic.branca@gmail.com>
2023-07-17 15:28:19 +0200

    delete useless lines

--- controller/HomeController.php
+++ controller/HomeController.php
@@ -6,8 +6,6 @@
     use App\AbstractController;
     use App\ControllerInterface;
     use Model\Managers\UserManager;
-    use Model\Managers\TopicManager;
-    use Model\Managers\PostManager;
     
     class HomeController extends AbstractController implements ControllerInterface{
 
