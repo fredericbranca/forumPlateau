919d9fda40dc3597a278f5233afd8ec4f0d91762
Frederic Branca <frederic.branca@gmail.com>
2023-07-17 14:08:07 +0200

    modify AbstractController (easy to use redirectTo)

--- app/AbstractController.php
+++ app/AbstractController.php
@@ -8,15 +8,13 @@
         public function redirectTo($ctrl = null, $action = null, $id = null){
 
             if($ctrl != "home"){
-                $url = $ctrl ? "/".$ctrl : "";
-                $url.= $action ? "/".$action : "";
-                $url.= $id ? "/".$id : "";
-                $url.= ".html";
-            }
-            else $url = "/";
+                $url = $ctrl ? "?ctrl=" . $ctrl : "";
+                $url.= $action ? "&action=" . $action : "";
+                $url.= $id ? "&id=" . $id : "";
+            } else $url = "/";
+
             header("Location: $url");
             die();
-
         }
 
         public function restrictTo($role){
