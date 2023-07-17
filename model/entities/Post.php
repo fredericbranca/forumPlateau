bd096555f9c30b4f0fc1fe68eb6d336d39b75c07
Frederic Branca <frederic.branca@gmail.com>
2023-07-17 11:38:36 +0200

    Create Post entities + getter & setter

---
+++ model/entities/Post.php
@@ -0,0 +1,117 @@
+<?php
+    namespace odel\Entities;
+
+    use App\Entity;
+
+
+    final class Post extends Entity {
+
+        private $id;
+        private $topic;
+        private $user;
+        private $message;
+        private $creationdate;
+
+        public function __construct($data) {
+            $this->hydrate($data);
+        }
+
+        /**
+         * Get the value of id
+         */ 
+        public function getId()
+        {
+                return $this->id;
+        }
+
+        /**
+         * Set the value of id
+         *
+         * @return  self
+         */ 
+        public function setId($id)
+        {
+                $this->id = $id;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of topic
+         */ 
+        public function getTopic()
+        {
+                return $this->topic;
+        }
+
+        /**
+         * Set the value of topic
+         *
+         * @return  self
+         */ 
+        public function setTopic($topic)
+        {
+                $this->topic = $topic;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of user
+         */ 
+        public function getUser()
+        {
+                return $this->user;
+        }
+
+        /**
+         * Set the value of user
+         *
+         * @return  self
+         */ 
+        public function setUser($user)
+        {
+                $this->user = $user;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of message
+         */ 
+        public function getMessage()
+        {
+                return $this->message;
+        }
+
+        /**
+         * Set the value of message
+         *
+         * @return  self
+         */ 
+        public function setMessage($message)
+        {
+                $this->message = $message;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of creationdate
+         */ 
+        public function getCreationdate(){
+            $formattedDate = $this->creationdate->format("d/m/Y, H:i:s");
+            return $formattedDate;
+        }
+
+        /**
+         * Set the value of creationdate
+         *
+         * @return  self
+         */ 
+        public function setCreationdate($date){
+            $this->creationdate = new \DateTime($date);
+            return $this;
+        }
+
+    }
