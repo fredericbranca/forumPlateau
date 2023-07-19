89a7f9079e97a4a650b931dc08bcfb1c94df4c82
Frederic Branca <frederic.branca@gmail.com>
2023-07-19 13:22:23 +0200

    form

--- controller/PostController.php
+++ controller/PostController.php
@@ -1,37 +1,35 @@
 <?php
 
-    namespace Controller;
+namespace Controller;
 
-    use App\Session;
-    use App\AbstractController;
-    use App\ControllerInterface;
-    use Model\Managers\PostManager;
-    use Model\Managers\TopicManager;
-    
-    class PostController extends AbstractController implements ControllerInterface{
+use App\Session;
+use App\AbstractController;
+use App\ControllerInterface;
+use Model\Managers\PostManager;
+use Model\Managers\TopicManager;
 
-        public function index() {
-            $this->redirectTo("topic");
-        }
+class PostController extends AbstractController implements ControllerInterface
+{
 
-
-        public function listPostsByTopic($id){
-          
-
-           $postManager = new PostManager();
-           $topicManager = new TopicManager();
-
-            return [
-                "view" => VIEW_DIR."forum\posts.php",
-                "data" => [
-                    "posts" => $postManager->findPostsById($_GET['id']),
-                    "topic" => $topicManager->findOneById($_GET['id'])
-                ]
-            ];
-        
-        }
-
-
-        
-
+    public function index()
+    {
+        $this->redirectTo("topic");
     }
+
+
+    public function listPostsByTopic($id)
+    {
+
+
+        $postManager = new PostManager();
+        $topicManager = new TopicManager();
+
+        return [
+            "view" => VIEW_DIR . "forum\posts.php",
+            "data" => [
+                "posts" => $postManager->findPostsById($id),
+                "topic" => $topicManager->findOneById($id)
+            ]
+        ];
+    }
+}
