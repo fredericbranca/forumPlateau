9c58ebb986d69caff57626c8fe506154cba39e28
Frederic Branca <frederic.branca@gmail.com>
2023-07-19 11:09:22 +0200

    creation method to find a post by Id

--- model/managers/PostManager.php
+++ model/managers/PostManager.php
@@ -2,6 +2,7 @@
     namespace Model\Managers;
     
     use App\Manager;
+    use App\DAO;
 
     class PostManager extends Manager{
 
@@ -13,5 +14,16 @@
             parent::connect();
         }
 
+        public function findPostsById($id){
+
+            $sql = "SELECT *
+                    FROM ".$this->tableName." a
+                    WHERE a.topic_id = :id";
+
+            return $this->getMultipleResults(
+                DAO::select($sql, ['id' => $id]),
+                $this->className
+            );
+        }
 
     }
