61b11157bba39db7d4113f47d13dd0be68f89fb3
Frederic Branca <frederic.branca@gmail.com>
2023-07-19 11:11:43 +0200

    create table for the list of topics

--- view/forum/listTopics.php
+++ view/forum/listTopics.php
@@ -1,21 +1,42 @@
 <?php
 
 $topics = $result["data"]['topics'];
-// var_dump($topics);
-// die();
 
 ?>
 
 <h1>liste topics</h1>
 
+<table class="topics-table">
+    <thead>
+        <tr>
+            <th>Discussion</th>
+            <th>Créé par</th>
+            <th>Créé le</th>
+            <th>Réponses</th>
+            <th>Dernier post</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        if (!empty($topics)) {
+            foreach ($topics as $topic) {
+        ?>
+                <tr onclick="document.location = 'index.php?ctrl=post&action=listPostsByTopic&id=<?= $topic->getId() ?>'">
+                    <td><?= $topic->getTitre() ?></td>
+                    <td><?= $topic->getUser() ?></td>
+                    <td><?= $topic->getCreationdate() ?></td>
+                    <td>nb réponse</td>
+                    <td>dernier post</td>
+                </tr>
+            <?php
+            }
+            ?>
+    </tbody>
+</table>
 <?php
-if (!empty($topics)) {
-    foreach ($topics as $topic) {
-    ?>
-        <p><?= $topic->getTitre()." ".$topic->getUser() ?></p>
-    <?php
-    }
-} else {
-    echo "Aucun topic existant.";
-}
+        } else {
+            echo "Aucun topic existant.";
+        }
+
+$style = "topics";
 ?>
