f4c417350abbb226838f1636fe1424b4b92aef42
Frederic Branca <frederic.branca@gmail.com>
2023-07-19 11:08:03 +0200

    postController method to display posts

--- controller/PostController.php
+++ controller/PostController.php
@@ -6,18 +6,26 @@
     use App\AbstractController;
     use App\ControllerInterface;
     use Model\Managers\PostManager;
+    use Model\Managers\TopicManager;
     
     class PostController extends AbstractController implements ControllerInterface{
 
-        public function index(){
+        public function index() {
+            $this->redirectTo("topic");
+        }
+
+
+        public function listPostsByTopic($id){
           
 
            $postManager = new PostManager();
+           $topicManager = new TopicManager();
 
             return [
-                "view" => VIEW_DIR."forum\post.php",
+                "view" => VIEW_DIR."forum\posts.php",
                 "data" => [
-                    "posts" => $postManager->findAll(["message", "DESC"])
+                    "posts" => $postManager->findPostsById($_GET['id']),
+                    "topic" => $topicManager->findOneById($_GET['id'])
                 ]
             ];
         
