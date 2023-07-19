e59804c479a2b596b6b05ffb53f42f22dfd767a7
Frederic Branca <frederic.branca@gmail.com>
2023-07-19 11:11:00 +0200

    add a link for each css

--- view/layout.php
+++ view/layout.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,11 +8,19 @@
     <script src="https://cdn.tiny.cloud/1/zg3mwraazn1b2ezih16je1tc6z7gwp5yd4pod06ae5uai8pa/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous" />
     <link rel="stylesheet" href="<?= PUBLIC_DIR ?>/css/style.css">
+    <?php
+    if (isset($style)) {
+    ?>
+        <link rel="stylesheet" href="<?= PUBLIC_DIR ?>/css/<?= $style ?>.css">
+    <?php
+    }
+    ?>
     <title>FORUM</title>
 </head>
+
 <body>
-    <div id="wrapper"> 
-       
+    <div id="wrapper">
+
         <div id="mainpage">
             <!-- c'est ici que les messages (erreur ou succès) s'affichent-->
             <h3 class="message" style="color: red"><?= App\Session::getFlash("error") ?></h3>
@@ -21,38 +30,37 @@
                     <div id="nav-left">
                         <a href="index.php">Accueil</a>
                         <?php
-                        if(App\Session::isAdmin()){
-                            ?>
+                        if (App\Session::isAdmin()) {
+                        ?>
                             <a href="index.php?ctrl=home&action=users">Voir la liste des gens</a>
-                          
-                            <?php
+
+                        <?php
                         }
                         ?>
                     </div>
                     <div id="nav-right">
-                    <?php
-                        
-                        if(App\Session::getUser()){
-                            ?>
-                            <a href="/security/viewProfile.html"><span class="fas fa-user"></span>&nbsp;<?= App\Session::getUser()?></a>
+                        <?php
+
+                        if (App\Session::getUser()) {
+                        ?>
+                            <a href="/security/viewProfile.html"><span class="fas fa-user"></span>&nbsp;<?= App\Session::getUser() ?></a>
                             <a href="/security/logout.html">Déconnexion</a>
-                            <?php
-                        }
-                        else{
-                            ?>
+                        <?php
+                        } else {
+                        ?>
                             <a href="./view/security/login.php">Connexion</a>
                             <a href="/security/register.html">Inscription</a>
                             <a href="index.php?ctrl=topic">La liste des Topics</a>
                             <a href="index.php?ctrl=categorie">La liste des Catégories</a>
                         <?php
                         }
-                   
-                        
-                    ?>
+
+
+                        ?>
                     </div>
                 </nav>
             </header>
-            
+
             <main id="forum">
                 <?= $page ?>
             </main>
@@ -62,22 +70,18 @@
             <!--<button id="ajaxbtn">Surprise en Ajax !</button> -> cliqué <span id="nbajax">0</span> fois-->
         </footer>
     </div>
-    <script
-        src="https://code.jquery.com/jquery-3.4.1.min.js"
-        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-        crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous">
     </script>
     <script>
-
-        $(document).ready(function(){
-            $(".message").each(function(){
-                if($(this).text().length > 0){
-                    $(this).slideDown(500, function(){
+        $(document).ready(function() {
+            $(".message").each(function() {
+                if ($(this).text().length > 0) {
+                    $(this).slideDown(500, function() {
                         $(this).delay(3000).slideUp(500)
                     })
                 }
             })
-            $(".delete-btn").on("click", function(){
+            $(".delete-btn").on("click", function() {
                 return confirm("Etes-vous sûr de vouloir supprimer?")
             })
             tinymce.init({
@@ -89,14 +93,14 @@
                     'insertdatetime media table paste code help wordcount'
                 ],
                 toolbar: 'undo redo | formatselect | ' +
-                'bold italic backcolor | alignleft aligncenter ' +
-                'alignright alignjustify | bullist numlist outdent indent | ' +
-                'removeformat | help',
+                    'bold italic backcolor | alignleft aligncenter ' +
+                    'alignright alignjustify | bullist numlist outdent indent | ' +
+                    'removeformat | help',
                 content_css: '//www.tiny.cloud/css/codepen.min.css'
             });
         })
 
-        
+
 
         /*
         $("#ajaxbtn").on("click", function(){
@@ -112,4 +116,5 @@
         })*/
     </script>
 </body>
+
 </html>
