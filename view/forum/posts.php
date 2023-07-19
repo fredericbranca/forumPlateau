1e3fed7a995d2e7de426cd03ff2ed0ac19bcf837
Frederic Branca <frederic.branca@gmail.com>
2023-07-19 11:12:11 +0200

    created posts of topic view

---
+++ view/forum/posts.php
@@ -0,0 +1,45 @@
+<?php
+
+$posts = $result["data"]['posts'];
+$topic = $result["data"]['topic'];
+
+?>
+
+<h1><?= $topic->getTitre() ?></h1>
+
+<div class="premierMessage">
+    <div><?= $topic->getUser() ?></div>
+    <div>le <?= $topic->getCreationdate() ?></div>
+    <div><?= $topic->getMessage() ?></div>
+</div>
+
+<?php
+if (!empty($posts)) {
+?>
+    <h2>Réponses</h2>
+    <div class="topicMessages">
+        <?php
+        foreach ($posts as $post) {
+        ?>
+            <div class="topicMessage">
+                <div><?= $post->getUser() ?></div>
+                <div>le <?= $post->getCreationdate() ?></div>
+                <div><?= $post->getMessage() ?></div>
+            </div>
+        <?php
+        }
+        ?>
+    </div>
+<?php
+} else {
+?>
+    <h2>Ce topic n'a pas encore de réponses</h2>
+<?php
+}
+?>
+
+
+
+<?php
+$style = "post";
+?>
