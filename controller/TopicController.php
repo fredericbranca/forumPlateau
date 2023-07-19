379afc4e873ac5a6910f09ebd8493126c69fac19
Frederic Branca <frederic.branca@gmail.com>
2023-07-19 11:09:00 +0200

    display topics order by creation date

--- controller/TopicController.php
+++ controller/TopicController.php
@@ -10,12 +10,23 @@ use Model\Managers\TopicManager;
 class TopicController extends AbstractController implements ControllerInterface
 {
 
-    public function index(){}
-    
-    public function listTopicsByCategorie($id) {
-        
+    public function index()
+    {
         $topicManager = new TopicManager();
-    
+
+        return [
+            "view" => VIEW_DIR . "forum\listTopics.php",
+            "data" => [
+                "topics" => $topicManager->findAll(["creationdate", "DESC"])
+            ]
+        ];
+    }
+
+    public function listTopicsByCategorie($id)
+    {
+
+        $topicManager = new TopicManager();
+
         return [
             "view" => VIEW_DIR . "forum\listTopics.php",
             "data" => [
