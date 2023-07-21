2c4fc37b19b525bd52ddbcd2e755fe4aff5b5d7a
Frederic Branca <frederic.branca@gmail.com>
2023-07-21 11:28:05 +0200

    answer Topic query

--- controller/PostController.php
+++ controller/PostController.php
@@ -19,10 +19,32 @@ class PostController extends AbstractController implements ControllerInterface
 
     public function listPostsByTopic($id)
     {
-
-
         $postManager = new PostManager();
         $topicManager = new TopicManager();
+
+        
+        // Répondre à un topic
+
+        if (isset($_POST['answerTopic']) && isset($_GET['id'])) {
+
+            // Filtres
+            $topicId = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+            $userId = filter_input(INPUT_POST, 'userID', FILTER_VALIDATE_INT);
+            $message = filter_input(INPUT_POST, 'message', FILTER_SANITIZE_SPECIAL_CHARS);
+            // Vérifie si les filtres sont ok
+            if ($topicId !== false && $userId !== false && $message !== false) {
+
+                $data = ['topic_id' => $topicId, 'user_id' => $userId, 'message' => $message];
+                // Ajoute le topic à la db grâce à la metho add() du Manager
+                $postManager->add($data);
+                // Redirection
+                $this->redirectTo('post', 'listPostsByTopic', $topicId);
+
+            } else {
+                SESSION::addFlash('error', "<div class='message'>Filtres non ok</div>");
+                $this->redirectTo('post', 'listPostsByTopic', $topicId);
+            }
+        }
 
         return [
             "view" => VIEW_DIR . "forum\posts.php",
