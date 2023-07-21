092cb109d2aa80c25f5fc49ef1cb6b2a8e99892b
Frederic Branca <frederic.branca@gmail.com>
2023-07-21 10:12:13 +0200

    create topic form

--- view/forum/createTopic.php
+++ view/forum/createTopic.php
@@ -1,7 +1,9 @@
-<form class="formulaire" method="POST" action="index.php?action=topic" enctype="multipart/form-data">
+<form class="formulaire" method="POST" action="index.php?ctrl=topic&action=createTopic" enctype="multipart/form-data">
 <input class="titre" type="text" id="titre" name="titre" maxlength="50" required autocomplete="off" placeholder="Titre de la discussion">
-<div class="post"></div>
-<button class="formulaire-btn" type="submit" name="submit" id="submit">Poster la discussion</button>
+<input class="post" name="message">
+<input type="hidden" name="userID" value="1">
+<input type="hidden" name="categorie" value="1">
+<button class="formulaire-btn" type="submit" name="createTopic" id="submit">Poster la discussion</button>
 </form>
 
 <?php
