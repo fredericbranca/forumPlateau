0a33644b72e9a9e7a7fa285dfe51fed94b61aee8
Frederic Branca <frederic.branca@gmail.com>
2023-07-24 14:58:52 +0200

    add categorie ASC  to the createTopic view form

--- view/forum/createTopic.php
+++ view/forum/createTopic.php
@@ -1,9 +1,25 @@
+<?php
+
+// Liste des topics ou liste des topics par catégorie
+$categories = $result["data"]['categories'];
+
+?>
+
 <form class="formulaire" method="POST" action="index.php?ctrl=topic&action=createTopic" enctype="multipart/form-data">
-<input class="titre" type="text" id="titre" name="titre" maxlength="50" required autocomplete="off" placeholder="Titre de la discussion">
-<input class="post" name="message">
-<input type="hidden" name="userID" value="1">
-<input type="hidden" name="categorie" value="1">
-<button class="formulaire-btn" type="submit" name="createTopic" id="submit">Poster la discussion</button>
+    <input class="titre" type="text" id="titre" name="titre" maxlength="50" required autocomplete="off" placeholder="Titre de la discussion">
+    <select name="categorie" id="categorie" required>
+        <option value="">--Choisir une catégorie--</option>
+        <?php
+        foreach ($categories as $categorie) {
+        ?>
+            <option value="<?= $categorie->getId() ?>"><?= $categorie->getNom() ?></option>
+        <?php
+        }
+        ?>
+
+    </select>
+    <input class="post" name="message">
+    <button class="formulaire-btn" type="submit" name="createTopic" id="submit">Poster la discussion</button>
 </form>
 
 <?php
