0d58184821bd2c45db3f64046d21d6d55b645062
Frederic Branca <frederic.branca@gmail.com>
2023-07-24 14:38:06 +0200

    Login function in the security controller

--- controller/SecurityController.php
+++ controller/SecurityController.php
@@ -43,7 +43,7 @@ class SecurityController extends AbstractController implements ControllerInterfa
                             $this->redirectTo("login");
                         // Si les mots de passe ne correspondent pas
                         } else {
-                            SESSION::addFlash('error', "<div class='message'>Les deux mots de passe ne correspondent pas</div>");
+                            SESSION::addFlash('error', "<div class='message'>Les deux mots de passe ne correspondent pas ou le mot de passe fait moins de 8 caractères</div>");
                             $this->redirectTo("security", "register");
                         }
                     // Si le nickname existe déjà
@@ -64,6 +64,63 @@ class SecurityController extends AbstractController implements ControllerInterfa
 
         return [
             "view" => VIEW_DIR . 'security\register.php'
+        ];
+    }
+
+    public function login() {
+
+        if (isset($_POST['submitLogin'])) {
+            // Filtre
+            $user = filter_input(INPUT_POST, "user", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            // On vérifie si c'est le nickname ou un email
+            // Patern email
+            $emailPattern = "/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix"; // Validation d'email par expression régulière
+            // preg_match() est utilisée pour rechercher un motif dans une chaîne de caractères
+            if (preg_match($emailPattern, $user)) {
+                $user = filter_input(INPUT_POST, "user", FILTER_SANITIZE_EMAIL, FILTER_VALIDATE_EMAIL);
+                $email = 1;
+            }
+            
+            if ($user !== false && $password !== false) {
+                $userManager = new UserManager();
+                // retrouver le mot de passe de l'utilisateur
+                $dbPass = $userManager->retrievePassword($user);
+                // si le mot de passe est retrouvé
+                if ($dbPass) {
+                    // récupération du mot de passe
+                    $hash = $dbPass->getPassword();
+                    // retrouver l'utilisateur
+                    if (isset($email)) {
+                        $user = $userManager->findOneByEmail($user);
+                    } else {
+                        $user = $userManager->findOneByUser($user);
+                    }
+                    // comparaison du hash de la base de données et le mot de passe renseigné
+                    if (password_verify($password, $hash)) {
+                        // si l'user n'est pas banni
+                        if (!$user->getStatut()) {
+                            // placer l'utilisateur en Session
+                            Session::setUser($user);
+                            SESSION::addFlash('success', "<div class='message'>Connexion réussie</div>");
+                            $this->redirectTo("index.php");
+                        } else {
+                            SESSION::addFlash('error', "<div class='message'>Ce compte est banni</div>");
+                            $this->redirectTo("security", "login");
+                        }
+                    } else {
+                        SESSION::addFlash('error', "<div class='message'>Mot de passe incorrect</div>");
+                        $this->redirectTo("security", "login");
+                    }
+                }  else {
+                    SESSION::addFlash('error', "<div class='message'>Erreur filtre</div>");
+                    $this->redirectTo("security", "register");
+                }
+            }
+        }
+
+        return [
+            "view" => VIEW_DIR . 'security\login.php'
         ];
     }
 }
