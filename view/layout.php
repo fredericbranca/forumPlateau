217e8174d0b6221d39f96b4e857eaf28b7f8f85d
Frederic Branca <frederic.branca@gmail.com>
2023-07-25 16:58:51 +0200

    user link

--- view/layout.php
+++ view/layout.php
@@ -45,7 +45,7 @@
                         ?>
                             <a href="index.php?ctrl=topic">La liste des Topics</a>
                             <a href="index.php?ctrl=categorie">La liste des Catégories</a>
-                            <a href="/security/viewProfile.html"><span class="fas fa-user"></span>&nbsp;<?= App\Session::getUser() ?></a>
+                            <a href="index.php?ctrl=security&action=user"><span class="fas fa-user"></span>&nbsp;<?= App\Session::getUser() ?></a>
                             <a href="index.php?ctrl=security&action=logout">Déconnexion</a>
                         <?php
                         } else {
