d4feb456206047dfc297250d2755e33695c1059e
Frederic Branca <frederic.branca@gmail.com>
2023-07-25 14:25:07 +0200

    retrieves the number of messages in the topic

--- model/managers/TopicManager.php
+++ model/managers/TopicManager.php
@@ -16,9 +16,12 @@
 
         public function findTopicsByCategorie($id){
 
-            $sql = "SELECT *
+            $sql = "SELECT a.*, COUNT(p.id_post) AS messagecount
                     FROM ".$this->tableName." a
-                    WHERE a.categorie_id = :id";
+                    LEFT JOIN post p ON p.topic_id = a.id_topic
+                    WHERE a.categorie_id = :id
+                    GROUP BY a.id_topic
+                    ORDER BY a.creationdate DESC";
 
             return $this->getMultipleResults(
                 DAO::select($sql, ['id' => $id]),
@@ -39,5 +42,19 @@
                 echo $e->getMessage();
                 die();
             }
+        }
+
+        public function findTopicMessageCounter() {
+
+            $sql = "SELECT t.*, COUNT(p.id_post) AS messagecount
+                    FROM topic t
+                    LEFT JOIN post p ON p.topic_id = t.id_topic
+                    GROUP BY t.id_topic
+                    ORDER BY t.creationdate DESC";
+
+            return $this->getMultipleResults(
+                DAO::select($sql), 
+                $this->className
+            );
         }
     } 
