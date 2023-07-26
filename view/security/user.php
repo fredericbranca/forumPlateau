3b485b711bdbd09b95a8bcd18fc0e29315e70770
Frederic Branca <frederic.branca@gmail.com>
2023-07-26 13:40:32 +0200

    access management, admin cannot delete admin user

--- view/security/user.php
+++ view/security/user.php
@@ -8,14 +8,19 @@ $user = $result["data"]['users'];
 
 <?php
 
-// convertit le statut : string en object DateTime
+// Vérifie si le statut est défini
 if ($user->getStatut()) {
+    // convertit le statut : string en object DateTime
     $statut = DateTime::createFromFormat('Y-m-d H:i:s', $user->getStatut());
 
     // Si la date du banissement est > à la date de maintenant, affiche jusqu'à quand l'user est banni, sinon affiche que le compte est actif
     if ($user->getStatut() !== "NULL" && $statut > new DateTime("now")) {
 ?>
         <p><u>Statut</u> : Compte banni jusqu'au <?= $user->getStatut() ?></p>
+    <?php
+    } else {
+    ?>
+        <p>Statut : Compte actif</p>
     <?php
     }
 } else {
@@ -24,7 +29,9 @@ if ($user->getStatut()) {
 <?php
 }
 
-if (App\Session::isAdmin()) {
+// Si l'utilisateur est admin il peut supprimer le compte de chaque utilisateur sauf le sien
+// L'utilisateur peut uniquement voir le bouton supprimer sur son profile ; un visiteur ne voit pas le bouton
+if (App\Session::getUser() && ((App\Session::isAdmin() && App\Session::getUser()->getId() !== $user->getId()) || (!App\Session::isAdmin() && (App\Session::getUser()->getId() === $user->getId())))) {
 ?>
     <form method="POST" action="index.php?ctrl=security&action=deleteUser&id=<?= $user->getId() ?>" enctype="multipart/form-data">
         <button class="delete-btn" type="submit" name="deleteUser" id="submit">Supprimer le compte</button>
