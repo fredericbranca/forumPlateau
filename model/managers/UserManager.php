f1f546c149f6ab1912eff50c632b82c5f9515e7b
Frederic Branca <frederic.branca@gmail.com>
2023-07-26 16:13:03 +0200

    method sql to ban a user for x seconds

--- model/managers/UserManager.php
+++ model/managers/UserManager.php
@@ -14,6 +14,7 @@
             parent::connect();
         }
 
+        // Récupérer un utilisateur grâce à son email
         public function findOneByEmail($email){
 
             $sql = "SELECT *
@@ -27,6 +28,7 @@
             );
         }
         
+        // Récupérer un utilisateur grâce à son pseudo
         public function findOneByUser($user){
 
             $sql = "SELECT *
@@ -40,6 +42,7 @@
             );
         }
 
+        // Récupérer le mot de passe d'un user grâce à son email ou son pseudo
         public function retrievePassword($user) {
 
             $sql = "SELECT password
@@ -53,4 +56,23 @@
             );
         }
 
+        // Bannir un utilisateur en seconde
+        public function bannir($id, $seconde) {
+            // DATE_ADD() ajoute un intervalle temps/date à une date et renvoie ensuite la date
+            $sql = "UPDATE ".$this->tableName."
+                    SET statut = 
+                        CASE
+                            WHEN statut IS NULL THEN DATE_ADD(NOW(), INTERVAL :seconde SECOND)
+                            ELSE DATE_ADD(NOW(), INTERVAL :seconde SECOND)
+                        END
+                    WHERE id_user = :id";
+
+            try{
+                return DAO::update($sql, ['id' => $id, 'seconde' => $seconde]);
+            }
+            catch(\PDOException $e){
+                echo $e->getMessage();
+                die();
+            }
+        }
     }
