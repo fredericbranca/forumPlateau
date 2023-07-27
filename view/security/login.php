bbd5c375249971d6ff8f6fcd230960e910a9939b
Frederic Branca <frederic.branca@gmail.com>
2023-07-27 23:47:56 +0200

    delete forgott password link

--- view/security/login.php
+++ view/security/login.php
@@ -2,6 +2,5 @@
 <form method="POST" action="index.php?ctrl=security&action=login" enctype="multipart/form-data">
     <input type="text" id="user" name="user" maxlength="100" required placeholder="Email / Pseudo" autocomplete="off">
     <input type="password" id="password" name="password" maxlength="255" required placeholder="Mot de passe" autocomplete="off">
-    <a href="#">Mot de passe oublié ?</a>
     <button class="bouton btn-login" type="submit" name="submitLogin" id="submit">Connexion</button>
 </form>
