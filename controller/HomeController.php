1ece1edd408f3b3d7dfc02cd5b5286e5ffc53fa8
Frederic Branca <frederic.branca@gmail.com>
2023-07-28 13:29:54 +0200

    change to find user by nickname asc

--- controller/HomeController.php
+++ controller/HomeController.php
@@ -23,10 +23,10 @@
             $this->restrictTo("ROLE_USER");
 
             $manager = new UserManager();
-            $users = $manager->findAll(['registerdate', 'DESC']);
+            $users = $manager->findAll(['nickname', 'ASC']);
 
             return [
-                "view" => VIEW_DIR."security/users.php",
+                "view" => VIEW_DIR."security/userslist.php",
                 "data" => [
                     "users" => $users
                 ]
