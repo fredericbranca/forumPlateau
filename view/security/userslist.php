7e2aad1e74bac1b1f1479a5695204e8eff4be6f4
Frederic Branca <frederic.branca@gmail.com>
2023-07-29 21:11:58 +0200

    userslist view

--- view/security/userslist.php
+++ view/security/userslist.php
@@ -1,6 +1,10 @@
 <?php
 $users = $result["data"]['users'];
+?>
 
+<h1>Utilisateurs inscrits</h1>
+
+<?php
 foreach($users as $user) {
 ?>
     <p><a href="index.php?ctrl=security&action=user&id=<?= $user->getId() ?>"><?= $user->getNickname() ?></a></p>
