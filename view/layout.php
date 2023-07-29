73920ecff0191b89fba82c5c545886bc664f4b53
Frederic Branca <frederic.branca@gmail.com>
2023-07-29 21:10:52 +0200

    changing the navbar markup

--- view/layout.php
+++ view/layout.php
@@ -1,21 +1,21 @@
 <?php
-    // Check si l'user en session existe dans la db
-    if (App\Session::getUser()) {
-        //Récupère l'id de l'user en session
-        $sessionUserId = App\Session::getUser()->getId();
-        // Utilise la method findOneById() du Manager
-        $userManager = new Model\Managers\UserManager;
-        $userExist = $userManager->findOneById($sessionUserId);
-        // Supprime la session si l'user existe pas
-        if (!$userExist) {
-            App\Session::unsetUser();
-            header("Location:index.php?ctl=topic");
-        }
+// Check si l'user en session existe dans la db
+if (App\Session::getUser()) {
+    //Récupère l'id de l'user en session
+    $sessionUserId = App\Session::getUser()->getId();
+    // Utilise la method findOneById() du Manager
+    $userManager = new Model\Managers\UserManager;
+    $userExist = $userManager->findOneById($sessionUserId);
+    // Supprime la session si l'user existe pas
+    if (!$userExist) {
+        App\Session::unsetUser();
+        header("Location:index.php?ctl=topic");
     }
+}
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
     <meta charset="UTF-8">
@@ -35,60 +35,55 @@
 </head>
 
 <body>
-    <div id="wrapper">
+    <div id="wrapper" data-theme="dark">
 
         <div id="mainpage">
-            <!-- c'est ici que les messages (erreur ou succès) s'affichent-->
-            <h3 class="message" style="color: red"><?= App\Session::getFlash("error") ?></h3>
-            <h3 class="message" style="color: green"><?= App\Session::getFlash("success") ?></h3>
             <header>
-                <nav>
-                    <div id="nav-left">
-                        <a href="index.php?ctrl=topic">Accueil</a>
-                        <?php
-                        if (App\Session::isAdmin()) {
-                        ?>
-                            <a href="index.php?ctrl=security&action=users">Voir la liste des utilisateurs</a>
-
-                        <?php
-                        }
-                        ?>
+                <div id="navbar">
+                    <div class="hamburger" onclick="toggleMenu()">
+                        <div class="line"></div>
+                        <div class="line"></div>
+                        <div class="line"></div>
                     </div>
-                    <div id="nav-right">
-                        <?php
-
-                        if (App\Session::getUser()) {
-                        ?>
-                            <a href="index.php?ctrl=topic">La liste des Topics</a>
-                            <a href="index.php?ctrl=categorie">La liste des Catégories</a>
-                            <a href="index.php?ctrl=security&action=user"><span class="fas fa-user"></span>&nbsp;<?= App\Session::getUser() ?></a>
-                            <a href="index.php?ctrl=security&action=logout">Déconnexion</a>
-                        <?php
-                        } else {
-                        ?>
-                            <a href="index.php?ctrl=security&action=login">Connexion</a>
-                            <a href="index.php?ctrl=security&action=register">Inscription</a>
-                            <a href="index.php?ctrl=topic">La liste des Topics</a>
-                            <a href="index.php?ctrl=categorie">La liste des Catégories</a>
-                        <?php
-                        }
-
-
-                        ?>
-                    </div>
-                </nav>
+                    <nav id="myNav" class="nav-links">
+                        <ul>
+                            <li><a href="index.php?ctrl=topic">Accueil</a></li>
+                            <li><a href="index.php?ctrl=categorie">La liste des Catégories</a></li>
+                            <?php
+                            if (App\Session::isAdmin()) {
+                            ?>
+                                <li><a href="index.php?ctrl=security&action=users">Voir la liste des utilisateurs</a></li>
+                            <?php 
+                            }
+                            if (App\Session::getUser()) { ?>
+                                <li><a href="index.php?ctrl=security&action=user"><span class="fas fa-user"></span>&nbsp;<?= App\Session::getUser() ?></a></li>
+                                <li><a href="index.php?ctrl=security&action=logout">Déconnexion</a></li>
+                            <?php
+                            } else { ?>
+                                <li><a href="index.php?ctrl=security&action=login">Connexion</a></li>
+                                <li><a href="index.php?ctrl=security&action=register">Inscription</a></li>
+                            <?php
+                            } ?>
+                        </ul>
+                    </nav>
+                </div>
             </header>
+            <div class="overlay"></div>
 
             <main id="forum">
+                <!-- c'est ici que les messages (erreur ou succès) s'affichent-->
+                <h3 class="message" style="color: red"><?= App\Session::getFlash("error") ?></h3>
+                <h3 class="message" style="color: green"><?= App\Session::getFlash("success") ?></h3>
                 <?= $page ?>
             </main>
         </div>
-        <footer>
+        <footer id="footer">
             <p>&copy; 2020 - <?= date('Y') ?> - Forum CDA - <a href="/home/forumRules.html">Règlement du forum</a> - <a href="">Mentions légales</a></p>
             <!--<button id="ajaxbtn">Surprise en Ajax !</button> -> cliqué <span id="nbajax">0</span> fois-->
         </footer>
     </div>
     <script type="text/javascript" src="<?= PUBLIC_DIR ?>/js/script.js"></script>
+    <script type="text/javascript" src="<?= PUBLIC_DIR ?>/js/forum-height.js"></script>
     <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous">
     </script>
     <script>
