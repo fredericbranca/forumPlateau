66727bb4a593ea0147d5e21f54773f4bc80709e5
Frederic Branca <frederic.branca@gmail.com>
2023-07-29 21:11:21 +0200

    from table to list topics view

--- view/forum/listTopics.php
+++ view/forum/listTopics.php
@@ -10,83 +10,67 @@ if (isset($result["data"]["categorie"])) {
 
 ?>
 
-<h1>Liste des topics
+<h1>Forums
     <?php
     // Affiche la catégorie si la page est trié par catégorie
     if (!empty($categorie)) {
-        echo " - Catégorie : " . $categorie->getNom();
-    }
-    ?>
+        echo " - " . $categorie->getNom();
+    } ?>
 </h1>
 
 <a href="index.php?ctrl=topic&action=createTopic">Créer une nouvelle discussion</a>
 <?php
 if (!empty($topics)) {
 ?>
-    <table class="topics-table">
-        <thead>
-            <tr>
-                <th>Discussion</th>
-                <th>Catégorie</th>
-                <th>Créé par</th>
-                <th>Créé le</th>
-                <th>Réponses</th>
-                <th>Dernier post</th>
-                <th>Statut</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            foreach ($topics as $topic) {
-                App\Session::renameOnlyAdmin($topic->getUser());
-            ?>
-                <tr onclick="document.location = 'index.php?ctrl=post&action=listPostsByTopic&id=<?= $topic->getId() ?>'">
-                    <td><p><?= $topic->getTitre() ?></p></td>
-                    <td><?= $topic->getCategorie() ?></td>
-                    <!-- Si l'user n'existe pas, affiche  "Utilisateur supprimé" sinon affiche l'user-->
-                    <td><?php if (!$topic->getUser()) {
-                            echo "Utilisateur supprimé";
-                        } else {
-                            echo $topic->getUser();
-                        } ?></td>
-                    <td><?= $topic->getCreationdate() ?></td>
-                    <td><?= $topic->getMessagecount() ?></td>
-                    <td><?= $topic->getLastPostDate() ? $topic->getLastPostDate() : "Pas de réponse" ?></td>
-                    <td>
-                        <?php
-                        if ($topic->getClosed()) {
-                        ?>
-                            Fermé
-                        <?php
-                        } else {
-                        ?>
-                            Ouvert
-                        <?php
-                        }
-                        ?>
-                    </td>
+    <div class="TopicTable">
+        <?php
+        foreach ($topics as $topic) {
+            App\Session::renameOnlyAdmin($topic->getUser());
+        ?>
+            <ul class="TopicList" onclick="document.location = 'index.php?ctrl=post&action=listPostsByTopic&id=<?= $topic->getId() ?>'">
+                <li>
+                    Titre : <?= $topic->getTitre() ?>
+                </li>
+                <li>
+                    Catégorie : <?= $topic->getCategorie() ?>
+                </li>
+                <li>
+                    <?php if (!$topic->getUser()) {
+                        echo "Utilisateur supprimé";
+                    } else {
+                        echo $topic->getUser();
+                    } ?>
+                </li>
+                <li>
+                    Créé le <?= $topic->getCreationdate() ?>
+                </li>
+                <li>
+                    <?= $topic->getMessagecount() < 1 ? "Aucune réponse" : $topic->getMessagecount() . " réponses" ?>
+                </li>
+                <!-- <li> -->
+                <?= $topic->getLastPostDate() ? "<li>Répondu le : " . $topic->getLastPostDate() . "</li>" : "" ?>
+                <!-- </li> -->
+                <li>
                     <?php
-                    if (App\Session::isAdmin()) {
-                    ?>
-                        <td>
-                            <!-- Bouton pour supprimer le topic -->
-                            <form method="POST" action="index.php?ctrl=topic&action=deleteTopic&id=<?= $topic->getId() ?>" enctype="multipart/form-data">
-                                <button class="delete-btn" type="submit" name="deleteTopic" id="submit">Supprimer le topic</button>
-                            </form>
-                        </td>
-                    <?php
-                    }
-                    ?>
-                </tr>
-            <?php
-            }
-            ?>
-        </tbody>
-    </table>
-<?php
-} else {
-?>
-    <div>Aucun topic existant dans cette catégorie</div>
-<?php
-}
-$style = "topics";
+                    if ($topic->getClosed()) { ?>
+                        Fermé
+                    <?php } else { ?>
+                        Ouvert
+                    <?php } ?>
+                </li>
+                <!-- Si l'user en session a le rôle admin -->
+                <?php if (App\Session::isAdmin()) { ?>
+                    <li>
+                        <!-- Bouton pour supprimer le topic -->
+                        <form method="POST" action="index.php?ctrl=topic&action=deleteTopic&id=<?= $topic->getId() ?>" enctype="multipart/form-data">
+                            <button class="delete-btn" type="submit" name="deleteTopic" id="submit">Supprimer le topic</button>
+                        </form>
+                    </li>
+                <?php } ?>
+            </ul>
+        <?php }
+    } else { ?>
+        <div>Aucun topic existant dans cette catégorie</div>
+    <?php }
+
+    $style = "topics";
