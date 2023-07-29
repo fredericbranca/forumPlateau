7ca10aaaf602bfefe16b60e6edcb49b95645dc0c
Frederic Branca <frederic.branca@gmail.com>
2023-07-29 21:11:36 +0200

    little changement

--- view/security/login.php
+++ view/security/login.php
@@ -1,6 +1,14 @@
-<div>CONNEXION</div>
+<h1>Connexion</h1>
 <form method="POST" action="index.php?ctrl=security&action=login" enctype="multipart/form-data">
     <input type="text" id="user" name="user" maxlength="100" required placeholder="Email / Pseudo" autocomplete="off">
     <input type="password" id="password" name="password" maxlength="255" required placeholder="Mot de passe" autocomplete="off">
     <button class="bouton btn-login" type="submit" name="submitLogin" id="submit">Connexion</button>
 </form>
+
+<h1>Pas encore inscrit ?</h1>
+<div class="not-register">
+    <a href="index.php?ctrl=security&action=register" class="btn-register-link">Inscription</a>
+</div>
+
+<?php
+$style = "register-login";
--- view/security/register.php
+++ view/security/register.php
@@ -1,4 +1,4 @@
-<div>INSCRIPTION</div>
+<h1>Inscription</h1>
 <form method="POST" action="index.php?ctrl=security&action=register" enctype="multipart/form-data">
     <input type="email" id="email" name="email" maxlength="100" required placeholder="Email" autocomplete="off">
     <input type="text" id="nickname" name="nickname" maxlength="15" required placeholder="Pseudo" autocomplete="off">
@@ -6,3 +6,12 @@
     <input type="password" id="confirmPassword" maxlength="255" name="confirmPassword" required placeholder="Confirmation du mot de passe" autocomplete="off">
     <button type="submit" name="submitSignUp" id="submit">Inscription</button>
 </form>
+
+<h1>Déjà inscrit ?</h1>
+<div class="registered">
+    <a href="index.php?ctrl=security&action=login" class="btn-connexion-link">Connexion</a>
+</div>
+
+<?php
+
+$style = "register-login";
