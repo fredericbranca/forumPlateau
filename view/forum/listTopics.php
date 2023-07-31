ed111cce7bf714fe9a5d05a60cbab6ec62ad1d36
Frederic Branca <frederic.branca@gmail.com>
2023-07-31 14:24:59 +0200

    adding div in the topics list view

--- view/forum/listTopics.php
+++ view/forum/listTopics.php
@@ -9,55 +9,50 @@ if (isset($result["data"]["categorie"])) {
 }
 
 ?>
+<div class="header">
+    <h1>Forums
+        <?php
+        // Affiche la catégorie si la page est trié par catégorie
+        if (!empty($categorie)) {
+            echo " - " . $categorie->getNom();
+        } ?>
+    </h1>
+    <a class="btn-link" href="index.php?ctrl=topic&action=createTopic">Nouvelle discussion</a>
+</div>
 
-<h1>Forums
-    <?php
-    // Affiche la catégorie si la page est trié par catégorie
-    if (!empty($categorie)) {
-        echo " - " . $categorie->getNom();
-    } ?>
-</h1>
-
-<a href="index.php?ctrl=topic&action=createTopic">Créer une nouvelle discussion</a>
 <?php
 if (!empty($topics)) {
 ?>
-    <div class="TopicTable">
-        <?php
-        foreach ($topics as $topic) {
-            App\Session::renameOnlyAdmin($topic->getUser());
-        ?>
-            <ul class="TopicList" onclick="document.location = 'index.php?ctrl=post&action=listPostsByTopic&id=<?= $topic->getId() ?>'">
-                <li>
-                    Titre : <?= $topic->getTitre() ?>
-                </li>
-                <li>
-                    Catégorie : <?= $topic->getCategorie() ?>
-                </li>
-                <li>
-                    <?php if (!$topic->getUser()) {
-                        echo "Utilisateur supprimé";
-                    } else {
-                        echo $topic->getUser();
-                    } ?>
-                </li>
-                <li>
-                    Créé le <?= $topic->getCreationdate() ?>
-                </li>
-                <li>
-                    <?= $topic->getMessagecount() < 1 ? "Aucune réponse" : $topic->getMessagecount() . " réponses" ?>
-                </li>
-                <!-- <li> -->
-                <?= $topic->getLastPostDate() ? "<li>Répondu le : " . $topic->getLastPostDate() . "</li>" : "" ?>
-                <!-- </li> -->
-                <li>
-                    <?php
-                    if ($topic->getClosed()) { ?>
-                        Fermé
-                    <?php } else { ?>
-                        Ouvert
-                    <?php } ?>
-                </li>
+    <div class="responsive-table">
+        <div class="topicTable">
+            <?php
+            foreach ($topics as $topic) {
+                App\Session::renameOnlyAdmin($topic->getUser());
+            ?>
+                <ul onclick="document.location = 'index.php?ctrl=post&action=listPostsByTopic&id=<?= $topic->getId() ?>'">
+                    <div class="header-topic">
+                        <li class="text-topic">
+                            <?= $topic->getCategorie() ?>
+                        </li>
+                        <li class="titre">
+                            <?= $topic->getClosed() ? '<i class="fa-sharp fa-solid fa-lock"></i> ' . $topic->getTitre() : '<i class="fa-solid fa-lock-open"></i> ' . $topic->getTitre() ?>
+                        </li>
+                        <li class="text-topic">Par
+                            <?php if (!$topic->getUser()) {
+                                echo "Utilisateur supprimé";
+                            } else {
+                                echo $topic->getUser();
+                            } ?>
+                            - <?= $topic->getCreationdate() ?>
+                        </li>
+                    </div>
+                    <div class="infos-topic">
+                        <li class="text-topic">
+                            <?= $topic->getMessagecount() < 1 ? "0 réponse" : $topic->getMessagecount() . " réponses" ?>
+                            <?= $topic->getLastPostDate() ? " - " . $topic->getLastPostDate() . "</li>" : "" ?>
+                            <!-- </li> -->
+                    </div>
+                </ul>
                 <!-- Si l'user en session a le rôle admin -->
                 <?php if (App\Session::isAdmin()) { ?>
                     <li>
@@ -67,10 +62,12 @@ if (!empty($topics)) {
                         </form>
                     </li>
                 <?php } ?>
-            </ul>
-        <?php }
-    } else { ?>
-        <div>Aucun topic existant dans cette catégorie</div>
-    <?php }
+            <?php }
+        } else { ?>
+            <div>Aucun topic existant dans cette catégorie</div>
+        <?php } ?>
+        </div>
+    </div>
 
+    <?php
     $style = "topics";
