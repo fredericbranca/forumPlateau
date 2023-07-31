309d38f01c4271da96b349097978e90d6fa78210
Frederic Branca <frederic.branca@gmail.com>
2023-07-31 14:24:35 +0200

    adding div in the categorie view

--- view/forum/listCategories.php
+++ view/forum/listCategories.php
@@ -1,15 +1,21 @@
 <?php
 
 $categories = $result["data"]['categories'];
-    
+
 ?>
 
 <h1>Catégories</h1>
 
-<?php
-foreach($categories as $categorie ){
+<div class="list-categorie">
+    <?php
+    foreach ($categories as $categorie) {
 
     ?>
-    <a href="index.php?ctrl=topic&action=listTopicsByCategorie&id=<?= $categorie->getId() ?>"><?=$categorie->getNom()?></a>
+        <a class="categorie" href="index.php?ctrl=topic&action=listTopicsByCategorie&id=<?= $categorie->getId() ?>"><span><?= $categorie->getNom() ?></span></a>
     <?php
-}
+    } ?>
+</div>
+
+<?php
+
+$style = "categorie";
